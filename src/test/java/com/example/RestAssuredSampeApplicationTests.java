9669031c24f9b998c93163d59bac47755650ec0b
hgmin777 <hgmin777@gmail.com>
2019-04-27 09:26:08 +0900

    initial

---
+++ src/test/java/com/example/RestAssuredSampeApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import io.restassured.RestAssured;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestAssuredSampeApplicationTests {
+
+	@Test
+	public void SampleTest() {
+		RestAssured.given()
+        .when().get("http://www.google.com")
+        .then().statusCode(200);
+	}
+
+	
+}
